d89f3fb6c9ef472e7662e7f8139e56423463105a
Sergiu104 <kalai.sergiu95@gmail.com>
2020-04-17 00:12:56 +0300

    Add JS functions

---
+++ Sergiu/appFolder/app.js
@@ -0,0 +1,9 @@
+const hamburger = document.querySelector("*.hamburger");
+const navLinks = document.querySelector("*.nav-links");
+const links = document.querySelector("*.nav-links-li");
+
+hamburger.addEventListener("click", () => {
+  navLinks.classList.toggle("open");
+});
+
+windows.blur(navBar);
